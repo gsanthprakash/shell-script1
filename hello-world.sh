166894311d88bd358409f70715361bc8d01761bb
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-13 21:28:08 -0500

    first commit

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,8 @@
-
+#!/bin/bash
 
 echo "hi, hello World ! i am learning DevOps"
+
+echo "Ramesh, HI Suresh, Good Morning"
+echo "Suresh, Good Morning Ramesh"
+echo "Ramesh, How are you"
+echo "Suresh, i am good, how are you Ramesh"
