b45db8fb53a79ccf1007d617391608f1b825bd16
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-13 22:01:01 -0500

    first commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,8 +3,8 @@
 # this is the commanet, we need to develop a program that can do addition of 2 numbers.
 
 NUMBER1=$1
-NUMBER1=$2
+NUMBER2=$2
 
 SUM=$((NUMBER1+NUMBER2))
 
-echo "addition of 2 numbers are :$SUM"
+echo "addition of 2 numbers are: $SUM"
