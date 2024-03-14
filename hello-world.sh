2929c542bff60610959387e83a081ec6569e088b
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-13 21:32:53 -0500

    first commit

--- hello-world.sh
+++ hello-world.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "hi, hello World ! i am learning DevOps"
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh, HI Suresh, Good Morning"
-echo "Suresh, Good Morning Ramesh"
-echo "Ramesh, How are you"
-echo "Suresh, i am good, how are you Ramesh"
+echo "$PERSON1, HI $PERSON2, Good Morning"
+echo "$PERSON2, Good Morning $PERSON1"
+echo "$PERSON1, How are you"
+echo "$PERSON2, i am good, how are you $PERSON1"
