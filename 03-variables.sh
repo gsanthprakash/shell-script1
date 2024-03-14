1edffb04c58820e0bbcc9ba7cacd54c8232a0245
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-13 21:50:45 -0500

    first commit

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "hi, hello World ! i am learning DevOps"
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1, HI $PERSON2, Good Morning"
+echo "$PERSON2, Good Morning $PERSON1"
+echo "$PERSON1, How are you"
+echo "$PERSON2, i am good, how are you $PERSON1"
