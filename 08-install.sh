532a1544c21d5a9d655b339e47258006cb66b7f3
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-14 19:45:31 -0500

    first commit

--- 08-install.sh
+++ 08-install.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "you are not root user, please proceed with root user"
+    echo "ERROR: you are not root user, please proceed with root user"
     exit 1
 else 
     echo "root user, please proceed"
