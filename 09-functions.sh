f9040bba33687db5e545efcc7458d8d5891ea85f
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-14 21:23:59 -0500

    messages

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$0
@@ -8,12 +11,22 @@ LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2 ...FAILURE"
+        echo "$2 ...$R FAILURE $N"
         exit 1
     else
-        echo "$2 ...SUCCESS"
+        echo "$2 ...$G SUCCESS $N"
     fi
 }
+
+USERID=(id u)
+
+if [ $USERID -ne 0 ]
+then    
+    echo "ERROR: Not root user"
+    exit 1
+else
+    echo "you are root user"
+fi
 
 # it is our responsibility again installation is success or not.
 yum install mysql -y  &>>$LOGFILE
