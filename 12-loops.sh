f0a59773293e55be21f9a763c03ca6fade90fdb8
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-14 22:57:50 -0500

    messages

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+DATE=(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIDR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
-SCRIPT_NAME=$0
-DATE=(date %d)
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
-USERID=$(id -u)
 if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR: please proceed with root user $N"
     exit 1
 else
-    echo - e "$G you are root user"
+    echo -e "$G you are root user"
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 .... $R FAILURE $N"
+        exi 1
+    else
+        echo -e "Installing $2 .... $G SUCCESS $N"
+}
 
 
 # all arguments are in $@ 
 for i in $@
 do 
-    yum install $i &>>$LOGFILE
+    yum list installed $i &>>$LOGFILE
+    else
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i" 
+    else
+        echo -e "$Y $i already installed"
 done
