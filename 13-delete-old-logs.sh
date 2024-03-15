0a10709aa9997fe6a65fa1ddf84626ba149a823c
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-14 23:37:32 -0500

    messages

---
+++ 13-delete-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/cenots/app-logs
+
+DATE(date +%F)
+LOGSDIR=/home/cenots/shell-script-logs
+SCRIPT_NAME=$0
+LOGSFILE=$LOGSDIR/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
