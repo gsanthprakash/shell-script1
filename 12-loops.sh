390b036093681516be50dc9065f42f7befc97474
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-14 22:30:05 -0500

    messages

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+R="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
+SCRIPT_NAME=$0
+DATE=(date %d)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ];
+then
+    echo -e " $R ERROR: please proceed with root user $N"
+    exit 1
+else
+    echo "you are root user:
+fi
+
+
+
 # all arguments are in $@ 
 for i in $@
 do 
