bdbdc1699994805b0e42631ed0e2ae1dc00e1bb6
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 21:34:44 -0500

    messages

--- 14-diskusage.sh
+++ 14-diskusage.sh
@@ -28,7 +28,7 @@ do
 
    if [[ $usage -gt $DISK_USAGE_THRESHOLD ]];
    then
-   message+="HIGH DISK USAGE ON $partition: $usage \n"
+   message+="HIGH DISK USAGE ON $partition: $usage%\n"
    fi
 
 done <<< $DISK_USAGE
