5d9bf1269ae0f2eeca5b8df58d431556ae2cf83a
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 21:00:24 -0500

    messages

--- 14-diskusage.sh
+++ 14-diskusage.sh
@@ -2,33 +2,26 @@
 
 DATE=$(date +%F)
 
-#colors
-#validations
-# logs redirdctions.
-
+# Colors
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-USERID=$(id -u)
-LOGSDIR=/home/centos/shell-script-logs
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+LOGSDIR="/home/centos/shell-script-logs"
+SCRIPT_NAME="$0"
+LOGFILE="$LOGSDIR/$(basename "$0")-$DATE.log"
 
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_USAGE_THRESHOLD=1% 
+DISK_USAGE=$(df -hT)
+DISK_USAGE_THRESHOLD=90
 
-#IFS= means internal feild seperator is space.
-while IFS= read line
-do 
-   usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-   partition=$(echo #line | awk '{print $1}')
-   # now you need to check wheather it is more than threshold or not.
+while IFS= read -r line; do
+    usage=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    partition=$(echo "$line" | awk '{print $7}')
 
-   if [ $usage -gt $DISK_USAGE_THRESHOLD ];
-   then 
-    message+="HIGH DISK USAGE ON $partition: $usage"
+    if [ "$usage" -gt "$DISK_USAGE_THRESHOLD" ]; then
+        message+="HIGH DISK USAGE ON $partition: $usage%\n"
+    fi
+done <<< "$DISK_USAGE"
 
-done >>> $DISK_USAGE
+echo -e "$message" | tee -a "$LOGFILE"
 
-echo "message: $message"
